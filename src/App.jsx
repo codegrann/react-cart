c5c7fe62598fb906c3e2a082887ec46e47e65354
codegrann <omondihills02@gmail.com>
2023-08-17 08:56:56 +0300

    filtered and rendered products according to size selected

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,48 @@ import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 
-const products=[
-    {
-      "id": 1,
-      "category": "shoes",
-      "image": "shoe1.jpg",
-      "name": "Hiker",
-      "price": 94.95,
-      "skus": [
-        { "sku": "17", "size": 7 },
-        { "sku": "18", "size": 8 }
-      ],
-      "description": "This rugged boot will get you up the mountain safely."
-    },
-    {
-      "id": 2,
-      "category": "shoes",
-      "image": "shoe2.jpg",
-      "name": "Climber",
-      "price": 78.99,
-      "skus": [
-        { "sku": "28", "size": 8 },
-        { "sku": "29", "size": 9 }
-      ],
-      "description": "Sure-footed traction in slippery conditions."
-    },
-    {
-      "id": 3,
-      "category": "shoes",
-      "image": "shoe3.jpg",
-      "name": "Explorer",
-      "price": 145.95,
-      "skus": [
-        { "sku": "37", "size": 7 },
-        { "sku": "38", "size": 8 },
-        { "sku": "39", "size": 9 }
-      ],
-      "description": "Look stylish while stomping in the mud."
-    }
-  ]
+const products = [
+  {
+    id: 1,
+    category: "shoes",
+    image: "shoe1.jpg",
+    name: "Hiker",
+    price: 94.95,
+    skus: [
+      { sku: "17", size: 7 },
+      { sku: "18", size: 8 },
+    ],
+    description: "This rugged boot will get you up the mountain safely.",
+  },
+  {
+    id: 2,
+    category: "shoes",
+    image: "shoe2.jpg",
+    name: "Climber",
+    price: 78.99,
+    skus: [
+      { sku: "28", size: 8 },
+      { sku: "29", size: 9 },
+    ],
+    description: "Sure-footed traction in slippery conditions.",
+  },
+  {
+    id: 3,
+    category: "shoes",
+    image: "shoe3.jpg",
+    name: "Explorer",
+    price: 145.95,
+    skus: [
+      { sku: "37", size: 7 },
+      { sku: "38", size: 8 },
+      { sku: "39", size: 9 },
+    ],
+    description: "Look stylish while stomping in the mud.",
+  },
+];
 
 export default function App() {
-  const [size, setSize]=useState("");
+  const [size, setSize] = useState("");
   function renderProduct(p) {
     return (
       <div key={p.id} className="product">
@@ -57,6 +57,10 @@ export default function App() {
     );
   }
 
+  const filteredProducts = size
+    ? products.filter((p) => p.skus.find((sku) => sku.size === parseInt(size)))
+    : products;
+
   return (
     <>
       <div className="content">
@@ -64,16 +68,18 @@ export default function App() {
         <main>
           <section id="filters">
             <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size" value={size} onChange={(e)=>setSize(e.target.value)}>
+            <select
+              id="size"
+              value={size}
+              onChange={(e) => setSize(e.target.value)}
+            >
               <option value="">All sizes</option>
               <option value="7">7</option>
               <option value="8">8</option>
               <option value="9">9</option>
             </select>
           </section>
-          <section id="products">
-            {products.map(renderProduct)}
-          </section>
+          <section id="products">{filteredProducts.map(renderProduct)}</section>
         </main>
       </div>
       <Footer />
